a318f1b6694d399996caf090b6447c2c18f5b282
193228 <193228@ids.upchiapas.edu.mx>
2022-02-27 17:28:55 -0600

    first commit

---
+++ algoritmo.py
@@ -0,0 +1,108 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from numpy.linalg import norm
+from metodos import *
+
+def randomsW(longitud):
+    w= []
+    for i in range(0,longitud-1):
+        w.append(np.random.uniform(0,1))
+    return w
+
+def etaR():
+    return np.random.uniform(0,1)
+
+def umbralR():
+    return np.random.uniform(0,1)
+
+def inicioR(X,Y,Wcero,umbral):
+    r =[]
+    for i in range(0, 5):
+        iteracion = entrenamiento(Wcero, etaR(), umbral, X, Y)
+        grafIteracion(iteracion)
+        r.append(iteracion)
+    graficacionEtas(r)
+    final = peso_final(r)
+    return final
+
+def inicio(Wcero,eta,umbral,X,Y):
+    r = entrenamiento(Wcero, eta, umbral, X, Y)
+    lista = grafIteracion(r)
+    return lista
+
+def entrenamiento(Wcero, eta, umbral, X, Y):
+    iteracion = 0
+    l = []
+    while True:
+        Wk = []
+        if (iteracion == 0):
+            Wk = Wcero
+        else:
+            Wk = wk.copy()
+
+        Uk = calcularU(X, Wk)
+        yc = yCalculada(Y, Uk)
+        error = calcularError(Y, yc)
+        ex = multiplicarEX(error, X)
+        p = multiplicacionEta(eta, ex)
+        wk = nuevoW(Wk, p)
+        norma = np.linalg.norm(error)
+        dic = {
+            "iteracion": iteracion,
+            "norma": norma,
+            "wk": Wk,
+            "eta": eta
+        }
+        l.append(dic)
+        iteracion += 1
+        try:
+            if norma < umbral:
+                break
+            else:
+                print("norma no es menor a umbral: ","norma es: ",norma," umbral es: ",umbral)
+                #break
+        except:
+            print("Ocurrio un ciclo")
+    return l
+
+def grafIteracion(lista):
+    df = pd.DataFrame(lista)
+    fig, ax = plt.subplots()
+    ax.plot(df.index.values, df["norma"])
+    plt.show()
+    return df
+
+
+def graficacionEtas(lista):
+    l = []
+    for i in lista:
+        df = pd.DataFrame(i)
+        mostrarGrafica = {
+            "iteracion": df.index.values,
+            "eta": df["eta"][0],
+            "norma": df["norma"],
+        }
+        l.append(mostrarGrafica)
+
+    df = pd.DataFrame(l)
+
+    plt.figure(figsize=[6, 6])
+    plt.plot(df["iteracion"][0], df["norma"][0], label="Eta numero1: " + str(df["eta"][0]))
+    plt.plot(df["iteracion"][1], df["norma"][1], label="Eta numero2: " + str(df["eta"][1]))
+    plt.plot(df["iteracion"][2], df["norma"][2], label="Eta numero3: " + str(df["eta"][2]))
+    plt.plot(df["iteracion"][3], df["norma"][3], label="Eta numero4: " + str(df["eta"][3]))
+    plt.plot(df["iteracion"][4], df["norma"][4], label="Eta numero5: " + str(df["eta"][4]))
+    plt.xlabel('Generaciones')  # override the xlabel
+    plt.ylabel('Fitness')  # override the ylabel
+    plt.title('Norma error')  # override the title
+    plt.legend()
+    plt.show()
+
+def peso_final(lista):
+    n = []
+    for i in lista:
+        dic = i.pop()
+        #print("eta:{0} pesosfinales {1} ".format(dic["eta"], dic["wk"]))
+        n.append([dic["eta"], dic["wk"]])
+    return n
---
+++ main.py
@@ -0,0 +1,92 @@
+import random
+import sys
+import PyQt5
+import pandas as pd
+import openpyxl
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QTableWidgetItem
+
+from algoritmo import *
+from vista.main import Ui_MainWindow as ventanaPrincipal
+X = []
+
+class MyApp(PyQt5.QtWidgets.QMainWindow, ventanaPrincipal):
+    def __init__(self):
+        PyQt5.QtWidgets.QMainWindow.__init__(self)
+        ventanaPrincipal.__init__(self)
+        self.setupUi(self)
+        acciones(self)
+
+def acciones(ventana):
+    ventana.botonValores.clicked.connect(lambda: leerArchivo(ventana))
+    ventana.botonResultados.clicked.connect(lambda: ajusteValores(ventana))
+    #ventana.aleatorioW.
+
+def leerArchivo(ventana):
+    eleccion = PyQt5.QtWidgets.QFileDialog.getOpenFileName(None, "Seleccione Archivo", "", "CSV FILES (*.xlsx)")
+    x = pd.read_excel(eleccion[0], sheet_name="X", header=None).to_numpy().tolist()
+    return X.append(x)
+
+def ajusteValores(ventana):
+    Y = []
+    if ventana.aleatorioW.isChecked():
+        w0 = randomsW(len(X[0]))
+    else:
+        w0 = ventana.valorW.text().split(',')
+
+    if ventana.aleatorioValores_2.isChecked():
+        for i in range(0,len(X[0])):
+            Y.append(random.randint(0, 1))
+    else:
+        valorY = ventana.valorY.text().split(',')
+        Y = [int(x) for x in valorY]
+
+    if ventana.aleatorioUmbral.isChecked():
+        umbral = umbralR()
+    else:
+        umbral = float(ventana.umbral.text())
+
+    if ventana.aleatorioAprendizaje.isChecked():
+        aprendizaje = etaR()
+    else:
+        aprendizaje = float(ventana.umbral_2.text())
+
+    if ventana.diversasTasas.isChecked():
+        graficacion = inicioR(X[0], Y, w0, umbral)
+        hacerTabla(ventana, graficacion)
+
+    if not ventana.diversasTasas.isChecked():
+        graficacion = inicio(w0,aprendizaje,umbral,X[0],Y)
+        hacerTabla2(ventana,graficacion)
+
+def hacerTabla2(ventana,lista):
+    ventana.tabla.setRowCount(1)
+    ventana.tabla.setItem(0,0,QtWidgets.QTableWidgetItem(str(lista["eta"].iloc[-1])))
+    ventana.tabla.setItem(0,1,QtWidgets.QTableWidgetItem(str(lista["wk"].iloc[-1])))
+
+
+
+def hacerTabla(ventana,lista):
+    l = []
+    for i in lista:
+        estado = str(i[0]), str(i[1])
+        l.append(estado)
+
+    ventana.tabla.setRowCount(0)  # limpia la tabla
+    #ventana.tabla.setEditTriggers(QtWidgets.QTableWidget.NoEditTriggers)  # no edita la tabla
+    fila = 0
+    for registro in l:
+        columna = 0
+        ventana.tabla.insertRow(fila)
+        for elemento in registro:
+            celda = QTableWidgetItem(elemento)
+            ventana.tabla.setItem(fila, columna, celda)
+            columna += 1
+        fila += 1
+
+if __name__ == '__main__':
+    app = PyQt5.QtWidgets.QApplication(sys.argv)  # crea un objeto de aplicación (Argumentos de sys)
+    window = MyApp()
+    window.show()
+    window.setFixedSize(window.size())
+    app.exec_()
